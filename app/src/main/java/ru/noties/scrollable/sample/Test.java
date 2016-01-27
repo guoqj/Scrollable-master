0761834de3abe972737077e0a1d3b77923e01c1b
guoqj <505848542@qq.com>
2016-01-27 10:46:10 +0800

    gq

--- app/src/main/java/ru/noties/scrollable/sample/Test.java
+++ app/src/main/java/ru/noties/scrollable/sample/Test.java
@@ -2,6 +2,7 @@ package ru.noties.scrollable.sample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 /**
  * Created by Administrator on 2016/1/27.
@@ -11,6 +12,7 @@ public class Test extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Button button;
     }
 
     @Override
