92c34fb127f2b78f034bf4a3be793546c38c2176
guoqj <505848542@qq.com>
2016-01-27 10:42:34 +0800

    gq

---
+++ app/src/main/java/ru/noties/scrollable/sample/Test.java
@@ -0,0 +1,25 @@
+package ru.noties.scrollable.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/1/27.
+ */
+public class Test extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
